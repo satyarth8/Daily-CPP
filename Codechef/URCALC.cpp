8491a53665ee33408ae45b811b3670a35d78f580
satyarth8 <satyarthaprakash7168@gmail.com>
2023-02-27 15:50:24 +0530

    added folder

---
+++ Codechef/URCALC.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+// URCALC Solution by Satyarth
+// fixed in cout returns the entire characteristic and mantissa part 
+int main() 
+{
+	double a,b;
+	cin>>a;//accept 1st num
+	cin>>b;//accept 2nd num
+	char c;
+	cin>>c;//accept operator
+	if(c=='+')
+	    cout<<a+b<<endl;
+	else if(c=='-')
+	    cout<<a-b<<endl;
+	else if(c=='/')
+	    cout<<fixed<<a/b<<endl;
+	else if(c=='*')
+	    cout<<a*b<<endl;
+	return 0;
+}
